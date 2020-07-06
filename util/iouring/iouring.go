eab2c6a162684be86e61d12d3ce67eed25a952c6
awzhgw <guowl18702995996@gmail.com>
2020-07-06 20:52:39 +0800

    Refactor: change iouring test code
    Signed-off-by: awzhgw <guowl18702995996@gmail.com>

--- util/iouring/iouring.go
+++ util/iouring/iouring.go
@@ -3,12 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
+	"github.com/hodgesds/iouring-go"
 	"log"
 	"math/rand"
-	"net"
-	"github.com/hodgesds/iouring-go"
-	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"path"
@@ -20,9 +17,6 @@ var (
 	size=flag.Int64("size",64,"default write size")
 	rootDir=flag.String("root","/data0","default write dir")
 	uring=flag.Bool("iouring",true,"is used iouring")
-	role=flag.String("role","server","default role")
-	addr1=flag.String("remote","127.0.0.1:8888","default remote addr")
-	port=8888
 	ring *iouring.Ring
 )
 
@@ -60,88 +54,10 @@ func main() {
 	flag.Parse()
 
 	inita()
-	if *role=="server"{
-		go func() {
-			e := http.ListenAndServe(fmt.Sprintf(":%v", 8822), nil)
-			if e != nil {
-				log.Println(fmt.Errorf("cannot listen pprof %v err %v", 8822, e))
-				os.Exit(1)
-			}
-		}()
-		l,err:=Listen()
-		if err!=nil {
-			log.Fatal(err)
-		}
-		for {
-			conn,err:=l.Accept()
-			if err!=nil {
-				log.Println(fmt.Sprintf("Accept error %v",err))
-				continue
-			}
-			go Write(conn)
-		}
-	}else {
-		go func() {
-			e := http.ListenAndServe(fmt.Sprintf(":%v", 8821), nil)
-			if e != nil {
-				log.Println(fmt.Errorf("cannot listen pprof %v err %v", 8822, e))
-				os.Exit(1)
-			}
-		}()
-		conn,err:=Connect(*addr1)
-		if err!=nil {
-			log.Fatalf("connect error %v",err)
-		}
-		data:=RandStringRunes(int(*size))
-		writeData:=([]byte)(data)
-		for {
-			_,err=conn.Write(writeData)
-			if err!=nil{
-				log.Println(fmt.Sprintf("write error %v",err))
-				return
-			}
-		}
-	}
-
+	Write()
 
 }
-
-func Listen()( l net.Listener,err error){
-	if *uring{
-		fmt.Printf("listening on port: %d\n", port)
-		l, err := ring.SockoptListener(
-			"tcp",
-			fmt.Sprintf(":%d", port),
-			func(err error) {
-				log.Println(err)
-			},
-			iouring.SOReuseport,
-		)
-		if err != nil {
-			log.Fatal(err)
-		}
-		return l,err
-	}else {
-		l,err=net.Listen("tcp",fmt.Sprintf(":%d", port))
-		return l,err
-	}
-}
-
-func Connect(addr string)(conn net.Conn,err error){
-	conn,err=net.DialTimeout("tcp",addr,time.Second)
-	if err!=nil {
-		log.Println(fmt.Sprintf("Dail to %v err %v",addr,err))
-		return
-	}
-	conn.(*net.TCPConn).SetNoDelay(true)
-	conn.(*net.TCPConn).SetLinger(0)
-	conn.(*net.TCPConn).SetKeepAlive(true)
-
-
-	return
-}
-
-func Write(conn net.Conn){
+func Write(){
 	dst, err := os.OpenFile(path.Join(*rootDir,"1.txt"), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -167,11 +83,6 @@ func Write(conn net.Conn){
 			log.Fatal(err)
 		}
 		for {
-			_,err=io.ReadFull(conn,data)
-			if err!=nil {
-				log.Fatalf("read from conn error %v",err)
-			}
-			fmt.Println(fmt.Sprintf("recive data %v",string(data)))
 			_,err=r.Write(data)
 			if err!=nil {
 				log.Fatalf("write error %v",err)
@@ -180,10 +91,6 @@ func Write(conn net.Conn){
 		}
 	}else {
 		for {
-			_,err=io.ReadFull(conn,data)
-			if err!=nil {
-				log.Fatalf("read from conn error %v",err)
-			}
 			_,err=dst.Write(data)
 			if err!=nil {
 				log.Fatalf("write error %v",err)
